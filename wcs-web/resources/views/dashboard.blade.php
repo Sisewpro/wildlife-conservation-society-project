1755541fc965a1c3fa3bc8b224c8b93cda0c1f52
Az Zikra Ismunandar <azzikra.ismunandar08@gmail.com>
2024-11-18 09:08:29 +0700

    Add files via upload

--- wcs-web/resources/views/dashboard.blade.php
+++ wcs-web/resources/views/dashboard.blade.php
@@ -2,7 +2,6 @@
     <div
         class="hero min-h-screen"
         style="background-image: url(https://images.unsplash.com/photo-1545063914-a1a6ec821c88?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdpbGRsaWZlfGVufDB8fDB8fHww);">
-        <!-- style="background-image: url(https://wallpapercave.com/wp/wp9196969.jpg);" -->
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-neutral-content text-center">
             <div class="max-w-md">
@@ -16,147 +15,137 @@
         </div>
     </div>
     
-    <h1 class="text-5xl font-bold mt-24 px-4 sm:px-6 lg:px-8 md:flex lg:flex">Recent Uploads</h1>
+    <h1 class="text-5xl font-bold mt-24 px-6 sm:px-8 lg:px-12 md:flex lg:flex">Recent Uploads</h1>
 
-    <div class="mx-auto px-4 sm:px-6 lg:px-8 md:flex lg:flex">
-        <div class="card bg-base-100 image-full w-96 shadow-xl mt-12 mx-5">
-            <figure>
-                <img
-                src="https://images.unsplash.com/photo-1543493311-14cc44c3ced4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMDYwNzA4MXx8ZW58MHx8fHx8"
-                alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div>
+    <div class="mx-auto px-14 sm:justify-items-center sm:px-16 lg:px-20 md:flex lg:flex">
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1542181149-585bcf0d9ce9?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMDYwNzA4MXx8ZW58MHx8fHx8') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
         </div>
 
-        <!-- <div class="card image-full bg-base-100 w-96 shadow-xl mt-12 mx-5">
-            <figure> -->
-                <img class="w-96 shadow-xl mt-12 mx-5 rounded-box"
-                src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Shoes" />
-            </figure>
-            <!-- <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div> -->
-        <!-- </div> -->
-
-        <div class="card bg-base-100 image-full w-96 shadow-xl mt-12 mx-5">
-            <figure>
-                <img
-                src="https://images.unsplash.com/photo-1456926631375-92c8ce872def?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdpbGRsaWZlfGVufDB8fDB8fHww"
-                alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div>
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
         </div>
 
-        <!-- <div class="card bg-base-100 image-full w-96 shadow-xl mt-12 mx-5">
-            <figure> -->
-                <img class="w-96 mt-12 mx-5 shadow-xl rounded-box"
-                src="https://images.unsplash.com/photo-1650810954198-757671d7d079?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Shoes" />
-            <!-- </figure> -->
-            <!-- <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div> -->
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1456926631375-92c8ce872def?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdpbGRsaWZlfGVufDB8fDB8fHww') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
+        </div>
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1650810954198-757671d7d079?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
         </div>
     </div>
+    
+    <div class="mx-auto px-14 sm:justify-items-center sm:px-16 lg:px-20 md:flex lg:flex">
 
-    <div class="mx-auto px-4 sm:px-6 lg:px-8 md:flex lg:flex">
-        <div class="card bg-base-100 image-full w-96 shadow-xl mt-12 mx-5">
-            <figure>
-                <img
-                src="https://images.unsplash.com/photo-1543493311-14cc44c3ced4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMDYwNzA4MXx8ZW58MHx8fHx8"
-                alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div>
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1542181149-585bcf0d9ce9?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMDYwNzA4MXx8ZW58MHx8fHx8') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
         </div>
 
-        <!-- <div class="card image-full bg-base-100 w-96 shadow-xl mt-12 mx-5">
-            <figure> -->
-                <img class="w-96 shadow-xl mt-12 mx-5 rounded-box"
-                src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Shoes" />
-            </figure>
-            <!-- <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div> -->
-        <!-- </div> -->
-
-        <div class="card bg-base-100 image-full w-96 shadow-xl mt-12 mx-5">
-            <figure>
-                <img
-                src="https://images.unsplash.com/photo-1456926631375-92c8ce872def?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdpbGRsaWZlfGVufDB8fDB8fHww"
-                alt="Shoes" />
-            </figure>
-            <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div>
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
         </div>
 
-        <!-- <div class="card bg-base-100 image-full w-96 shadow-xl mt-12 mx-5">
-            <figure> -->
-                <img class="w-96 mt-12 mx-5 shadow-xl rounded-box"
-                src="https://images.unsplash.com/photo-1650810954198-757671d7d079?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D"
-                alt="Shoes" />
-            <!-- </figure> -->
-            <!-- <div class="card-body">
-                <div class="card-actions justify-center pt-16">
-                    <button class="btn">View</button>
-                </div>
-                <div class="justify-start">
-                    <h2 class="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                </div>
-            </div> -->
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1456926631375-92c8ce872def?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdpbGRsaWZlfGVufDB8fDB8fHww') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
+        </div>
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1650810954198-757671d7d079?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
         </div>
     </div>
+    
+    <div class="mx-auto px-14 sm:justify-items-center sm:px-16 lg:px-20 md:flex lg:flex">
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1542181149-585bcf0d9ce9?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnwxMDYwNzA4MXx8ZW58MHx8fHx8') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose? If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
+        </div>
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8fA%3D%3D') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
+        </div>
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1456926631375-92c8ce872def?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdpbGRsaWZlfGVufDB8fDB8fHww') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
+        </div>
+
+        <div class="card w-96 mt-12 mx-5">
+            @component('components.card')
+                @slot('imageUrl', 'https://images.unsplash.com/photo-1650810954198-757671d7d079?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D') <!-- Ganti dengan URL gambar -->
+                @slot('title', 'Shoes!')
+                @slot('description', 'If a dog chews shoes whose shoes does he choose?')
+                @slot('date', '2024-11-13')
+                @slot('location', 'New York')
+            @endcomponent
+        </div>
+    </div>
+    
 
     <div class="mt-24 pb-4 text-center"><p>Provided by Techno Net Copyright 2024.</p></div>
 </x-app-layout>
